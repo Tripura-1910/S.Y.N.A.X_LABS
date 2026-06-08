544df13d85e10524c2ff6328999dde6c0bacc8dc
Samuel T Thomas <samueltthomas2002@gmail.com>
2026-06-08 11:56:38 +0530

    Added Google AdSense integration

---
+++ frontend/src/components/AdsenseAd.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+const AdsenseAd = () => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="pub-1427277154820976"
+      data-ad-slot="1124418556"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
+  );
+};
+
+export default AdsenseAd;
